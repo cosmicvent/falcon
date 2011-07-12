cc7fdb733ab43d59ddb7bc28d8b186be9f457622
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2011-07-12 20:21:41 +0530

    installed responders and cancan

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Falcon::Application.routes.draw do
-  :root :to => 'home#index'
+  root :to => 'home#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
