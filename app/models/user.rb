d1181df317f50953627a2f54870b71de462e9256
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2011-07-12 20:33:59 +0530

    added devise user and views

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User
+  include Mongoid::Document
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :encryptable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+end
