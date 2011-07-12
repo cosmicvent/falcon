03314c26421f2972daa56bfdd84f38fef5ea5712
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2011-07-12 19:55:05 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Falcon::Application.config.secret_token = '2d79e9fd758b78dd2f041ddf0b0f42cd98a0fa8a8e8ef4c81e645a35e13a7c60252fb1762912153d5879059654124cb9956b15060bd84e74c312e87da89c193d'
