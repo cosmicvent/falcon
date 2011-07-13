d41bbd9ad5501825fc8fad537f20abe3f6c4bde5
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2011-07-13 15:12:04 +0530

    added issue specs

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,33 +1,24 @@
 class IssuesController < ApplicationController
-  # GET /issues
-  # GET /issues.xml
   def index
-    @issues = Issue.all
     @project = Project.find(params[:project_id])
+    @issues = @project.issues
     respond_with(@issues)
   end
 
-  # GET /issues/1
-  # GET /issues/1.xml
   def show
     @issue = Issue.find(params[:id])
     respond_with(@issue)
   end
 
-  # GET /issues/new
-  # GET /issues/new.xml
   def new
     @issue = Issue.new(:project => Project.find(params[:project_id]))
     respond_with(@issue)
   end
 
-  # GET /issues/1/edit
   def edit
     @issue = Issue.find(params[:id])
   end
 
-  # POST /issues
-  # POST /issues.xml
   def create
     @issue = Issue.new(params[:issue])
     @issue.owner = current_user
@@ -36,16 +27,12 @@ class IssuesController < ApplicationController
     respond_with(@issue.project, @issue)
   end
 
-  # PUT /issues/1
-  # PUT /issues/1.xml
   def update
     @issue = Issue.find(params[:id])
     @issue.update_attributes(params[:issue])
     respond_with(@issue.project, @issue)
   end
 
-  # DELETE /issues/1
-  # DELETE /issues/1.xml
   def destroy
     @issue = Issue.find(params[:id])
     @issue.destroy
--- spec/requests/issues_spec.rb
+++ spec/requests/issues_spec.rb
@@ -14,6 +14,47 @@ describe "Issues" do
       @project.issues.each do |issue|
         page.should have_content(issue.title)
       end
+
+    end
+
+  end
+
+  describe "create" do
+    it "should be able to create an issue" do
+
+      @project = Factory(:project)
+      initial_count = @project.issues.count
+      visit new_project_issue_path(@project)
+
+      fill_in "Title", with: "My first issue"
+      fill_in "Description", with: "Some awesome issue"
+      click_on "Create Issue"
+
+      page.should have_content("Issue was successfully created.")
+
+      @project.reload
+      @project.issues.count.should == ( initial_count + 1 )
+
     end
   end
+
+  describe "create" do
+    it "should be able to create an issue" do
+
+      @project = Factory(:project)
+      @issue = @project.issues.first
+      visit edit_project_issue_path(@project, @issue)
+
+      fill_in "Title", with: "Here is a changed title"
+      fill_in "Description", with: "and a changed description"
+      click_on "Update Issue"
+
+      page.should have_content("Issue was successfully updated.")
+
+      @issue.reload
+      @issue.title.should == "Here is a changed title"
+      @issue.description.should ==  "and a changed description"
+    end
+  end
+
 end
