8a47b78d1521fc22c1d98b3b8a580ccdf8e78fa1
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2011-07-13 07:28:05 +0530

    fixed routing specs

--- spec/routing/issues_routing_spec.rb
+++ spec/routing/issues_routing_spec.rb
@@ -3,33 +3,36 @@ require "spec_helper"
 describe IssuesController do
   describe "routing" do
 
-    #it "routes to #index" do
-      #get("/issues").should route_to("issues#index")
-    #end
+    def pid; "4e1c69f6ce0d49339000000d";end
+    def iid; "4e1c69f6ce0d49339000000e";end
 
-    #it "routes to #new" do
-      #get("/issues/new").should route_to("issues#new")
-    #end
+    it "routes to #index" do
+      get("/projects/#{pid}/issues").should route_to("issues#index", :project_id => pid)
+    end
 
-    #it "routes to #show" do
-      #get("/issues/1").should route_to("issues#show", :id => "1")
-    #end
+    it "routes to #new" do
+      get("/projects/#{pid}/issues/new").should route_to("issues#new", :project_id => pid)
+    end
 
-    #it "routes to #edit" do
-      #get("/issues/1/edit").should route_to("issues#edit", :id => "1")
-    #end
+    it "routes to #show" do
+      get("/projects/#{pid}/issues/#{iid}").should route_to("issues#show", :id => iid, :project_id => pid)
+    end
 
-    #it "routes to #create" do
-      #post("/issues").should route_to("issues#create")
-    #end
+    it "routes to #edit" do
+      get("/projects/#{pid}/issues/#{iid}/edit").should route_to("issues#edit", :id => iid, :project_id => pid)
+    end
 
-    #it "routes to #update" do
-      #put("/issues/1").should route_to("issues#update", :id => "1")
-    #end
+    it "routes to #create" do
+      post("/projects/#{pid}/issues").should route_to("issues#create", :project_id => pid)
+    end
 
-    #it "routes to #destroy" do
-      #delete("/issues/1").should route_to("issues#destroy", :id => "1")
-    #end
+    it "routes to #update" do
+      put("/projects/#{pid}/issues/#{iid}").should route_to("issues#update", :id => iid, :project_id => pid)
+    end
+
+    it "routes to #destroy" do
+      delete("/projects/#{pid}/issues/#{iid}").should route_to("issues#destroy", :id => iid, :project_id => pid)
+    end
 
   end
 end
